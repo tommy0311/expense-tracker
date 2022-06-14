05cb01da5a3f9329db10abff331d02ee2c9b6406
litangchen <chen.li.tang@gmail.com>
2022-06-14 22:44:09 +0800

    update: optimize route/home

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const lodash = require('lodash');
 
 const CategoryModel = require('../../models/category')
 const RecordModel = require('../../models/record')
 
 router.get("/", (req, res) => {
   const userId = req.user._id
-  let categoryTitle = ""
+  let category = {
+    "id": 0,
+    "name":  "全部"
+  }
   const categoryId = req.query.categoryId ? Number(req.query.categoryId) : 0
   const findObj = categoryId ? { userId, categoryId } : { userId }
 
   CategoryModel.findOne({ "id": categoryId })
-  .then( category => {
-    category ? categoryTitle = category.name : categoryTitle = "全部"
+  .lean()
+  .then( obj => {
+    if(obj){
+      category = lodash.cloneDeep(obj)
+    }
   })
   .then(() => {
     return RecordModel.find( findObj )
@@ -23,7 +30,7 @@ router.get("/", (req, res) => {
       for (const [index, record] of records.entries()){
         totalAmount = totalAmount + record.amount
       }
-      res.render("index", { records, categoryId, categoryTitle, totalAmount})
+      res.render("index", { records, category, totalAmount})
     })
     .catch(error => console.log(error))
   })
